6594e69ec72fecad71e93145c6094ede36d8ca3f
Fluffy-Lion <leoncooper31@gmail.com>
2021-02-02 19:04:39 +0000

    useraccount services updated

---
+++ src/services/userAccount.js
@@ -0,0 +1,92 @@
+const logIn= (email, password) => {
+    console.log('LogIn - Email: '+email+', Password: '+password);
+        firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((user) =>
+        {
+            alert('user signed in')
+            userSignedIn = true
+            console.log(userSignedIn)
+            show()
+        }).catch((error) =>{
+            alert("user not signed in")
+            show()
+        })
+}
+
+const register = (email, password) => {
+    console.log('Register - Email: '+email+', Password: '+password);
+    firebase.auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) =>
+    {
+        alert('user account created')
+    }).catch((error) => {
+        alert("user cannot be created")
+    })
+}
+const getProfileData = () => {
+    let user = firebase.auth().currentUser;
+    if(user != null){
+        console.log(' user = ', user)
+    } else {
+        alert("there is not authenticated user")
+    }
+}
+
+const logout = () => {
+    firebase.auth().signOut().then(() =>{
+        alert('user signed out')
+        userSignedIn = false
+        console.log(userSignedIn)
+        show()
+    }).catch((error) => {
+        alert('something went wrong')
+    })
+}
+
+const updateProfile = () => {
+    var user = firebase.auth().currentUser
+    if(user != null){
+    user.updateProfile({
+    displayName: "updated name"
+    })
+
+    user.updateEmail("test@updated.com").then(() => {
+        alert('email updated');
+    }).catch((error) => {
+        alert('email not updated');
+    })
+    
+    }else{
+    alert('there is no user')
+    }
+}
+const verifyUser = () => {
+    let user = firebase.auth().currentUser;
+    user.sendEmailVerification().then(() => {
+        alert('email sent')
+    }).catch('email not sent')
+}
+
+const deleteUser = () => {
+    let user = firebase.auth().currentUser;
+    user.delete().then(() => {
+        alert('user deleted')
+    }).catch((error) => {
+        alert('user not deleted')
+    })
+}
+
+const resetPassword = () => {
+    let user = firebase.auth().currentUser;
+    firebase.auth().sendPasswordResetEmail(user.email).then
+        (() => {
+            alert('email sent')
+        }).catch('email not sent')
+}
+
+const msg = () => {
+    console.log('hello there')
+}
