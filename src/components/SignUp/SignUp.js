1285e00d0f964f118a4d5774220e0f61f798b453
Fluffy-Lion <leoncooper31@gmail.com>
2021-02-10 22:55:06 +0000

    error with higher order component with sign up, needs check

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import './SignUp.css'
-import { FirebaseContext } from '../Firebase'
+// import { FirebaseContext } from '../Firebase'
+import { Link, withRouter } from 'react-router-dom'
 import SignUpForm from './SignUpForm'
 
+import { withFirebase } from '../Firebase'
+import * as ROUTES from '../../services/routes'
 const SignUp = () => {
 
 
@@ -10,10 +13,10 @@ const SignUp = () => {
     return (
         <div className="signUpWrapper">
             <h1>sign up</h1>
-            <FirebaseContext.Consumer>{firebase =>
+            {/* <FirebaseContext.Consumer>{firebase => */}
                 <SignUpForm />
-                }
-            </FirebaseContext.Consumer>
+                {/* }
+            </FirebaseContext.Consumer> */}
             {/* {registerResult ?
             <div>successful</div>
             : <div>unable to process</div>
@@ -22,5 +25,11 @@ const SignUp = () => {
     )
         
 }
+const SignUpLink = () => (
+    <p>
+        sign up now! <Link to={ROUTES.SIGN_UP}>SIGN UP</Link>
+    </p>
+)
+const SignUpPage = withRouter(withFirebase(SignUpForm))
 
-export default SignUp
+export default { SignUpPage, SignUpLink }
