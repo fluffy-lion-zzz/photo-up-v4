b14cc8f5edaf35a4b526bb9349d5afda560fdeb2
Fluffy-Lion <leoncooper31@gmail.com>
2021-02-14 13:54:59 +0000

    user auth for login status, testing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Home.css'
 import SomeComponent from '../SomeComponent'
 
+import {AuthContext} from '../../services/AuthContext'
+
 const Home = () => {
+
+    const authValue = useContext(AuthContext)
+    console.log(authValue)
     return (
         <div className="homeWrapper">
             <h1>home</h1>
             <SomeComponent />
+            <div>
+           {authValue.user == null ? <p>not logged</p> :
+           <p>user logged in</p>}
+            </div>
         </div>
     )
 }
