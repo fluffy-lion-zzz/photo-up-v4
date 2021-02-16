a0f52f04cb3d31ac26f5518a7e122f7aa50d6ecf
Fluffy-Lion <leoncooper31@gmail.com>
2021-02-16 15:39:08 +0000

    private route sorted

--- src/services/PrivateRoute.js
+++ src/services/PrivateRoute.js
@@ -5,20 +5,18 @@ import UserContext  from './AuthContext'
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
     const user = useContext(UserContext)
-    console.log(user)
+    // console.log(user)
     return (
         <Route
         {...rest}
         render={routeProps => 
-            !user.loggedIn ? (
+            user.loggedIn ? (
                 <RouteComponent {...routeProps} />
         ) : (
             <Redirect to={'/login'} />
         )
         }
-
         />
-        
     )
 }
 
