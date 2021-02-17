1dfc6d4ee99d27c5a00f0251b139b2279bb03a73
Fluffy-Lion <leoncooper31@gmail.com>
2021-02-17 13:50:38 +0000

    listing in console items in folder

--- src/components/ViewPhotos/ViewPhotos.js
+++ src/components/ViewPhotos/ViewPhotos.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react'
+
 import app from 'firebase/app'
 import "firebase/storage"
 // import { useState } from 'react'
+import './ViewPhotos.css'
 
 const ViewPhotos = () => {
 const [imageUrl, setImageUrl] = useState("")
+const [collected, setCollected] = useState("")
 
 const tester = () => {
     app.storage().ref('prelude.jpg').getDownloadURL().then((url) => {
         setImageUrl(url)
     })
+}
+const lister = () => {
+    console.log(app.storage().ref('test/').listAll())
     
 }
-
     return(
-        <div>
+        <div id="viewPhotos">
             <h3>view photos component</h3>
-            <img src={imageUrl} />
+            <div id="individualImgWrapper">
+                <img id="individualImg" src={imageUrl} />
+            </div>
+            <div>
+                
+            </div>
             <button onClick={tester}>test</button>
+            <button onClick={lister}>lister</button>
         </div>
     )
 }
