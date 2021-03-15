77ec58f615b66bf33991024efed95f0331f89017
Fluffy-Lion <leoncooper31@gmail.com>
2021-03-15 21:32:12 +0000

    added toggle for meta input

--- src/components/AddMeta/AddMeta.js
+++ src/components/AddMeta/AddMeta.js
@@ -8,10 +8,16 @@ const AddMeta = ({ photoRef }) => {
     console.log("photp ref: ", photoRef.fullPath) :
     console.log("not photo ref")
     const [amount, setAmount] = useState(0)
+    const [oneDisplay, setOneDisplay] = useState(true)
+    const [twoDisplay, setTwoDisplay] = useState(false)
+    const [threeDisplay, setThreeDisplay] = useState(false)
+    const [fourDisplay, setFourDisplay] = useState(false)
+    const [fiveDisplay, setFiveDisplay] = useState(false)
+
     const [meta, setMeta] = useState({
         
         customMetadata: {
-            customMetaOne: "testing",
+            customMetaOne: "",
             customMetaTwo: "",
             customMetaThree: "",
             customMetaFour: "",
@@ -38,12 +44,34 @@ const AddMeta = ({ photoRef }) => {
         })
     }
 
-    const TagInput = () => {
-
+    const oneHandler = () => {
+        addCustomMeta()
+        setOneDisplay(false)
+        setTwoDisplay(true)
+    }
+    const twoHandler = () => {
+        addCustomMeta()
+        setTwoDisplay(false)
+        setThreeDisplay(true)
+    }
+    const threeHandler = () => {
+        addCustomMeta()
+        setThreeDisplay(false)
+        setFourDisplay(true)
+    }
+    const fourHandler = () => {
+        addCustomMeta()
+        setFourDisplay(false)
+        setFiveDisplay(true)
+    }
+    const fiveHandler = () => {
+        addCustomMeta()
+        setFiveDisplay(false)
     }
     return (
         <div>
         <h1>add meta</h1>
+        { oneDisplay ? 
         <div>
             <input
             type="text"
@@ -58,9 +86,13 @@ const AddMeta = ({ photoRef }) => {
             }}
             >
             </input>
-            <button onClick={addCustomMeta}>confirm</button>
+            <button onClick={oneHandler}>confirm</button>
         <p>meta1= {meta.customMetadata.customMetaOne}</p>
         </div>
+        :
+        <><p>first tag set</p></>
+        }
+        { twoDisplay ?
         <div>
             <input
             type="text"
@@ -75,9 +107,13 @@ const AddMeta = ({ photoRef }) => {
             }}
             >
             </input>
-            <button onClick={addCustomMeta}>confirm</button>
+            <button onClick={twoHandler}>confirm</button>
         <p>meta2= {meta.customMetadata.customMetaTwo}</p>
         </div>
+        :
+        <><p>2nd tag set</p></>
+        }
+        { threeDisplay ?
         <div>
             <input
             type="text"
@@ -92,9 +128,13 @@ const AddMeta = ({ photoRef }) => {
             }}
             >
             </input>
-            <button onClick={addCustomMeta}>confirm</button>
+            <button onClick={threeHandler}>confirm</button>
         <p>meta3= {meta.customMetadata.customMetaThree}</p>
         </div>
+        :
+        <><p>3rd set</p></>
+        }
+        { fourDisplay ?
         <div>
             <input
             type="text"
@@ -109,9 +149,13 @@ const AddMeta = ({ photoRef }) => {
             }}
             >
             </input>
-            <button onClick={addCustomMeta}>confirm</button>
+            <button onClick={fourHandler}>confirm</button>
         <p>meta4= {meta.customMetadata.customMetaFour}</p>
         </div>
+        :
+        <><p>4th set</p></>
+        }
+        { fiveDisplay ?
         <div>
             <input
             type="text"
@@ -126,10 +170,12 @@ const AddMeta = ({ photoRef }) => {
             }}
             >
             </input>
-            <button onClick={addCustomMeta}>confirm</button>
+            <button onClick={fiveHandler}>confirm</button>
         <p>meta5= {meta.customMetadata.customMetaFive}</p>
         </div>
-
+        :
+        <><p>5th set</p></>
+        }
         <button onClick={addCustomMeta}>meta tester</button>
 
         <div>
