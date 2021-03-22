32a7c180c9d473addc5d8471f875c8ed91ab593a
Fluffy-Lion <leoncooper31@gmail.com>
2021-03-22 21:36:47 +0000

    writing undo function for meta

--- src/components/AddMeta/AddMeta.js
+++ src/components/AddMeta/AddMeta.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react'
+import React, { useState, useEffect }from 'react'
 import './AddMeta.css'
 import app from'firebase/app'
 import 'firebase/storage'
@@ -9,12 +9,17 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
     photoRef !== "" ?
     console.log("photp ref: ", photoRef.fullPath) :
     console.log("not photo ref")
-    const [oneDisplay, setOneDisplay] = useState(true)
-    const [twoDisplay, setTwoDisplay] = useState(false)
-    const [threeDisplay, setThreeDisplay] = useState(false)
-    const [fourDisplay, setFourDisplay] = useState(false)
-    const [fiveDisplay, setFiveDisplay] = useState(false)
+    // const [oneDisplay, setOneDisplay] = useState(true)
+    // const [twoDisplay, setTwoDisplay] = useState(false)
+    // const [threeDisplay, setThreeDisplay] = useState(false)
+    // const [fourDisplay, setFourDisplay] = useState(false)
+    // const [fiveDisplay, setFiveDisplay] = useState(false)
 
+    const [metaTagOne, setMetaTagOne] = useState("")
+    const [metaTagTwo, setMetaTagTwo] = useState("")
+    const [metaTagThree, setMetaTagThree] = useState("")
+    const [metaTagFour, setMetaTagFour] = useState("")
+    const [metaTagFive, setMetaTagFive] = useState("")
     const [meta, setMeta] = useState({
         
         customMetadata: {
@@ -47,58 +52,101 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
 
     const oneHandler = () => {
         addCustomMeta()
-        setOneDisplay(false)
-        setTwoDisplay(true)
+        let metaOne = meta.customMetadata.customMetaOne
+        setMetaTagOne(metaOne)
+
+        // setOneDisplay(false)
+        // setTwoDisplay(true)
+
     }
     const twoHandler = () => {
         addCustomMeta()
-        setTwoDisplay(false)
-        setThreeDisplay(true)
+        let metaTwo = meta.customMetadata.customMetaTwo
+        setMetaTagTwo(metaTwo)
+
+        // setTwoDisplay(false)
+        // setThreeDisplay(true)
+
     }
     const threeHandler = () => {
         addCustomMeta()
-        setThreeDisplay(false)
-        setFourDisplay(true)
+        let metaThree = meta.customMetadata.customMetaThree
+        setMetaTagThree(metaThree)
+
+        // setThreeDisplay(false)
+        // setFourDisplay(true)
+
     }
     const fourHandler = () => {
         addCustomMeta()
-        setFourDisplay(false)
-        setFiveDisplay(true)
+        let metaFour = meta.customMetadata.customMetaFour
+        setMetaTagFour(metaFour)
+
+        // setFourDisplay(false)
+        // setFiveDisplay(true)
+
     }
     const fiveHandler = () => {
         addCustomMeta()
-        setFiveDisplay(false)
-        setShowAddMeta(false)
+        let metaFive = meta.customMetadata.customMetaFive
+        setMetaTagFive(metaFive)
+
+        // setFiveDisplay(false)
+        // setShowAddMeta(false)
+
         viewReset()
     }
 
     const Director = (props) => {
+        console.log(props)
+        let reset = props.stateReset("")
         return(
             <div>
                 <button onClick={props.handler}>confirm</button>
-                <button>undo</button>
+                <button onClick={() => reset}>undo</button>
             </div>
         )
+        
     }
+    useEffect(() => {
+
+    }, [])
     const AddedTags = () => {
-        let metaOne = meta.customMetadata.customMetaOne
-        console.log(metaOne)
-        let metaTwo = meta.customMetadata.customMetaTwo
-        let metaThree = meta.customMetadata.customMetaThree
-        let metaFour = meta.customMetadata.customMetaFour
-        let metaFive = meta.customMetadata.customMetaFive
+
         return (
             <div>
                 <div>
-                    <>{metaOne !== "" ?
-                    <>{metaOne}</> :
+                    <>{metaTagOne !== "" ?
+                    <>{metaTagOne}</> :
                     <></>
                     }
                     </>
                 </div>
                 <div>
-                    <>{metaTwo !== "" ?
-                    <>{metaTwo}</> :
+                    <>{metaTagTwo !== "" ?
+                    <>{metaTagTwo}</> :
+                    <></>
+                    }
+                    </>
+                </div>
+
+                <div>
+                    <>{metaTagThree !== "" ?
+                    <>{metaTagThree}</> :
+                    <></>
+                    }
+                    </>
+                </div>
+                <div>
+                    <>{metaTagFour !== "" ?
+                    <>{metaTagFour}</> :
+                    <></>
+                    }
+                    </>
+                </div>
+                <div>
+                    <>{metaTagFive !== "" ?
+                    <>{metaTagFive}</> :
                     <></>
                     }
                     </>
@@ -111,7 +159,9 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
         <h1>add meta</h1>
         <MainMetaTags />
         <AddedTags />
-        { oneDisplay ? 
+
+        {/* { oneDisplay ?  */}
+        
         <div>
             <input
             type="text"
@@ -126,13 +176,16 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
             }}
             >
             </input>
-            <button onClick={oneHandler}>confirm</button>
+            <Director 
+                stateReset={setMetaTagOne}
+                handler={oneHandler} 
+            />
         <p>meta1= {meta.customMetadata.customMetaOne}</p>
         </div>
-        :
+        {/* :
         <><p>first tag set</p></>
         }
-        { twoDisplay ?
+        { twoDisplay ? */}
         <div>
             <input
             type="text"
@@ -150,10 +203,10 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
             <button onClick={twoHandler}>confirm</button>
         <p>meta2= {meta.customMetadata.customMetaTwo}</p>
         </div>
-        :
+        {/* :
         <><p>2nd tag set</p></>
         }
-        { threeDisplay ?
+        { threeDisplay ? */}
         <div>
             <input
             type="text"
@@ -171,10 +224,10 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
             <button onClick={threeHandler}>confirm</button>
         <p>meta3= {meta.customMetadata.customMetaThree}</p>
         </div>
-        :
+        {/* :
         <><p>3rd set</p></>
         }
-        { fourDisplay ?
+        { fourDisplay ? */}
         <div>
             <input
             type="text"
@@ -192,10 +245,10 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
             <button onClick={fourHandler}>confirm</button>
         <p>meta4= {meta.customMetadata.customMetaFour}</p>
         </div>
-        :
+        {/* :
         <><p>4th set</p></>
         }
-        { fiveDisplay ?
+        { fiveDisplay ? */}
         <div>
             <input
             type="text"
@@ -213,9 +266,9 @@ const AddMeta = ({ photoRef, setShowAddMeta, viewReset }) => {
             <button onClick={fiveHandler}>confirm</button>
         <p>meta5= {meta.customMetadata.customMetaFive}</p>
         </div>
-        :
+        {/* :
         <><p>5th set</p></>
-        }
+        } */}
         <button onClick={addCustomMeta}>finish tags</button>
 
         <div>
