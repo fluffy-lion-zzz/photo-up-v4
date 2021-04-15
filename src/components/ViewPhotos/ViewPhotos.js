c3a8f8f293968efba40b175fc5357ec6e791b20d
Fluffy-Lion <leoncooper31@gmail.com>
2021-04-15 14:33:30 +0100

    view photos clean

--- src/components/ViewPhotos/ViewPhotos.js
+++ src/components/ViewPhotos/ViewPhotos.js
@@ -52,67 +52,10 @@ const ViewPhotos = ({ storageRef, storage }) => {
         loader()
         console.log(imgData.length)
     }, [currentFolder])
-    // const [folderSelect, setFolderSelect] = useState("")
-    
-    // storageRef && storageRef.list().then(res => {
-    //     /* res.prefixes.map(item => {
-    //         folder.includes(item.name) ?
-    //             console.log("added") :
-    //             setFolder([...folder, item.name])
-    //     }) */
-    //     console.log(res.prefixes)
-    // })
-    // console.log("folders : ", folder)
-
-    
-
-    // const ViewImages = () => {
-    //     // storage.ref(folder + '/').list().then(res => {
-    //     //     res.items.map((item) => {
-    //     //         item.getDownloadURL().then((url) => {
-    //     //             // console.log(url)
-    //     //             return (
-    //     //                 <div>
-    //     //                     <img src={url}></img>
-    //     //                 </div>
-    //     //             )
-    //     //         })
-    //     //     })
-    //     // })
-    //     // >>>>>>>>
-    //     // storage.ref(currentFolder + '/').list().then(res => {
-    //     //     res.items.map((item) => {
-    //     //         item.getDownloadURL().then((url) => {
-    //     //             console.log(url)
-    //     //             imgData.includes(url) ? 
-    //     //             console.log("url already added") :
-    //     //            setImgData(imgData => [...imgData, url])
-    //     //         })
-    //     //     })
-    //     // })
-    //     // >>>>>>>>
-
-    //     storage.ref(currentFolder + '/').list().then(res => {
-    //         res.items.map(item => {
-    //             item.getDownloadURL().then((url) => {
-    //                 setImgData([...imgData, url])
-    //             })
-    //         })
-    //     })
-    //     console.log(imgData)
-    //     return (
-    //         <div>
-    //             {/* {imgData.map((url) => {
-    //                 return <img src={url} />
-    //             })} */}
-    //             <p>new img</p>
-    //         </div>
-    //     )
-
-    // }
 
     return (
         <div className="imgWrapper">
+            <h3>select a folder : </h3>
             <Selecter 
                 currentFolder={currentFolder}
                 folders={folders}
@@ -122,102 +65,15 @@ const ViewPhotos = ({ storageRef, storage }) => {
             />
             {/* <ViewImages /> */}
             <br/>
-           
-                {imgData.map(url => {
-                return (
-                    <div className="imagesContainer">
-                        <img className="imageSep" src={url} />
-                    </div>
-                )
-                })}
-            
+            {imgData.map(url => {
+            return (
+                <div className="imagesContainer">
+                    <img className="imageSep" src={url} />
+                </div>
+            )
+            })}
         </div>
     )
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>
-    // const viewFolder = () => {
-    //     const storage = app.storage()
-    //     const storageRef = storage.ref()
-    //     storageRef.list().then(res => {
-    //         res.prefixes.map((prefixes) => {
-    //             folder.includes(prefixes.name) ?
-    //             console.log("folder name already included") :
-    //             setFolder(folder => [...folder, prefixes.name])
-    //         })
-    //     })
-    // }
-    // let getData = () => {
-    //     setImgData([])
-    //     const storage = app.storage()
-    //     const storageRef = storage.ref(folderSelect +'/')
-    //     const newImg = storageRef.list()
-    //     newImg.then((res) => {
-    //         res.items.map((item)=> {
-    //             item.getDownloadURL().then((url) => {
-    //                 imgData.includes(url) ?
-    //                     console.log('url already there') :
-    //                     setImgData(imgData => [...imgData, url])
-    //             })
-    //         })
-    //         setLoading(false)
-    //     }) 
-
-
-    // }
-    // const ListDisplay = ({ url }) => {
-    //     return (
-    //         <div className="imgWrapper">
-    //             <img src={url}/>
-    //         </div>
-    //     )
-    // }
-    // const folderHandler = (event) => {
-    //     event.preventDefault()
-    //     let selected = event.target.value
-    //     setFolderSelect(selected)
-    //     console.log(selected)
-    // }
-    // const SelectFolder = () => {
-    //     return (
-    //         <div>
-    //             <form>
-    //                 <label for="selectAFolder">select a folder</label>
-    //                 <select onChange={folderHandler} name="selectAFolder">
-    //                     {folder.map((item, index) => {
-    //                     return <option key={index}>{item}</option>
-    //                 })}
-    //                 </select>
-
-    //             </form>
-    //         </div>
-    //     )
-    // }
-
-    // useEffect(() => {
-    //         getData()
-    //         viewFolder()
-    //         console.log("hit : ", folderSelect)
-
-    // },[])
-
-    // return(
-    //     <div id="viewPhotos">
-    //         <h3>view photos component</h3>
-    //         <SelectFolder />
-    //         <div className="imagesContainer">
-
-    //             {loading ? 
-    //             <Loading />
-    //             : (
-    //             imgData.map(url => {
-    //                 return <ListDisplay url={url} />
-    //             })
-    //             )
-    //             }
-
-
-    //         </div>
-    //     </div>
-    // )
 }
 
 export default ViewPhotos
