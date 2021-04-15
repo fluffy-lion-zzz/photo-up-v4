561ecd71d8c21c7ea83640754e923f6ab678c6ee
Fluffy-Lion <leoncooper31@gmail.com>
2021-04-15 12:02:30 +0100

    added sign up link

--- src/components/HowTo/HowTo.js
+++ src/components/HowTo/HowTo.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import './HowTo.css'
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../../services/routes'
 const HowTo = () => {
 
     return(
-        <h2>how to</h2>
+        <div className="howToCont">
+            <h2>how to</h2>
+            <div>
+                <h3>sign up</h3>
+                <p>make sure you are signed up or logged in</p>
+                <p>member of the team? not signed up yet...why? 
+                    <br />
+                    <Link to={ROUTES.SIGN_UP}>sign up</Link>
+                </p>
+            </div>
+        </div>
     )
 }
 
